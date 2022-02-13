491bd4c4902e6ac14eb7ee4b3ecdcb87ae56d5e7
Pavel Kalinin <pkalinin15@gmail.com>
2022-02-13 18:24:12 +0300

    feature(master): refactored, embed colors

--- src/main/java/by/pklabs/aormiobot/service/MessageSender.java
+++ src/main/java/by/pklabs/aormiobot/service/MessageSender.java
@@ -6,12 +6,15 @@ import net.dv8tion.jda.api.entities.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.awt.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class MessageSender {
     private static final Logger logger = LoggerFactory.getLogger(MessageSender.class);
+    private final String TIME_FORMAT = "dd/MM/yy HH:mm:ss";
     private TextChannel channel;
+
     public MessageSender(TextChannel channel){
         this.channel = channel;
     }
@@ -19,45 +22,49 @@ public class MessageSender {
     public void sendMutedMessage(User user, User moderator, String reason, long time,
                                     String timeEnd, boolean isUpdate){
         EmbedBuilder embed = new EmbedBuilder();
+        embed.setColor(Color.ORANGE);
         embed.setAuthor(user.getName() + "#" + user.getDiscriminator(), null, user.getAvatarUrl())
                 .setTitle(isUpdate ? "Обновлено время мута" : "Пользователю выдан мут")
                 .setDescription(user.getAsMention() + " получил мут на " + time + " " + timeEnd)
                 .addField("Причина", reason, false)
                 .setFooter(moderator.getName() + " | "
-                                + LocalDateTime.now().plusHours(3).format(DateTimeFormatter.ofPattern("MM/dd/yy HH:mm:ss"))
+                                + LocalDateTime.now().plusHours(3).format(DateTimeFormatter.ofPattern(TIME_FORMAT))
                         , moderator.getAvatarUrl());
         channel.sendMessageEmbeds(embed.build()).queue();
     }
 
     public void sendUnmutedMessage(User user, User moderator){
         EmbedBuilder embed = new EmbedBuilder();
+        embed.setColor(Color.GREEN);
         embed.setAuthor(user.getName() + "#" + user.getDiscriminator(), null, user.getAvatarUrl())
                 .setTitle("Пользователю был снят мут")
                 .setDescription("С пользователя " + user.getAsMention() + " был снят мут модератором")
                 .setFooter(moderator.getName() + " | "
-                                + LocalDateTime.now().plusHours(3).format(DateTimeFormatter.ofPattern("MM/dd/yy HH:mm:ss"))
+                                + LocalDateTime.now().plusHours(3).format(DateTimeFormatter.ofPattern(TIME_FORMAT))
                             , moderator.getAvatarUrl());
         channel.sendMessageEmbeds(embed.build()).queue();
     }
 
     public void sendAutoUnmutedMessage(User user){
         EmbedBuilder embed = new EmbedBuilder();
+        embed.setColor(Color.GREEN);
         embed.setAuthor(user.getName() + "#" + user.getDiscriminator(), null, user.getAvatarUrl())
                 .setTitle("Автоматическое снятие мута")
                 .setDescription("С пользователя " + user.getAsMention() + " был снят мут")
                 .setFooter("AORMIO BOT" + " | "
-                        + LocalDateTime.now().plusHours(3).format(DateTimeFormatter.ofPattern("MM/dd/yy HH:mm:ss")));
+                        + LocalDateTime.now().plusHours(3).format(DateTimeFormatter.ofPattern(TIME_FORMAT)));
         channel.sendMessageEmbeds(embed.build()).queue();
     }
 
     public void sendBanMessage(User user, User moderator, String reason){
         EmbedBuilder embed = new EmbedBuilder();
+        embed.setColor(Color.RED);
         embed.setAuthor(user.getName() + "#" + user.getDiscriminator(), null, user.getAvatarUrl())
                 .setTitle("Пользователь забанен")
                 .setDescription(user.getAsMention() + " получил бан")
                 .addField("Причина", reason, false)
                 .setFooter(moderator.getName() + " | "
-                                    + LocalDateTime.now().plusHours(3).format(DateTimeFormatter.ofPattern("MM/dd/yy HH:mm:ss"))
+                                    + LocalDateTime.now().plusHours(3).format(DateTimeFormatter.ofPattern(TIME_FORMAT))
                             , moderator.getAvatarUrl());
         channel.sendMessageEmbeds(embed.build()).queue();
     }
